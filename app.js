b7ac8e40cd3e76da6e0bfadac2564cba75274437
AngelDark92 <angelodark92@gmail.com>
2023-05-24 12:39:35 +0200

    Completed the peer assignement
    Previously misunderstood the requirements...

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 (function () {
   // to use only locally declared variables
-'use strict';
+  'use strict';
 
-// create the angular module
-angular.module('TavernettaLogin', [])
-// and its controller
-.controller("Login", Login);
+  // create the angular module
+  angular.module('InputBox', [])
+    // and its controller
+    .controller("InputBoxController", InputBoxController);
 
-// inject the scope to the controller
-Login.$inject = ["$scope"];
-// Define the function with one nested function for checking the username
-function Login($scope) {
-  $scope.name = "";
-  $scope.present = " empty";
+  // inject the scope to the controller
+  InputBoxController.$inject = ["$scope"];
+  function InputBoxController($scope) {
+    $scope.input = "";
+    $scope.check = "Please enter data first";
 
-  $scope.checkIfPresent = function () {
-    var present = fill($scope.name);
-    $scope.present = present;
+    $scope.checkIfPresent = function () {
+      var check = checkArray($scope.input);
+      $scope.check = check;
+    };
+
+
+    function checkArray(string) {
+      var arrayStrings = string.split(";");
+      var count = 0;
+      arrayStrings.filter(item => item.trim() != "").forEach(item =>
+        {
+          count++;
+        });
+      if (!(count > 0)) {
+        return "Please enter data first";
+      }
+      else if (count <= 3) {
+        return "Enjoy!";
+      }
+      else {
+        return "Too much!";
+      }
+    }
+
   };
-
-
-function fill(string) {
-    if(string.trim().toUpperCase() === "AngeloDark92".toUpperCase())
-    {
-      return " present";
-    }
-    if (string == null || string.trim() === '')
-    {
-      return " empty";
-    }
-    return " not present";
-  }
-
-};
 
 
 })();
